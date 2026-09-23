dad4bf21b534d31403bf2056066318d4d7915ec9
unknown <CodeKyng>
2026-09-23 13:49:57 +0100

    My first Commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2.75,
+  slidesPerGroup: 1,
+  speed: 600,
+  coverflowEffect: {
+    rotate: 10,
+    stretch: 0,
+    depth: 100,
+    modifier: 3,
+    slideShadows: true,
+  },
+  loop: true,
+  rewind: true,
+  loopAdditionalSlides: 3,
+  loopPreventsSliding: false,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: false,
+  },
+});
